5d3958d7aa66b8927643c81d82de4c6c735108a5
michael <mgutensohn@rollins.edu>
2016-11-20 19:42:23 -0500

    Added and implemented cryptor class. Cryptor uses AES-256 level encryption. This requires installation of JCE Unlimited Strength Jurisdiction Policy to be installed.

--- Documents/CMS341/Appcryptor/app/src/main/java/cms341/appcryptor/MainActivity.java
+++ Documents/CMS341/Appcryptor/app/src/main/java/cms341/appcryptor/MainActivity.java
@@ -2,12 +2,41 @@ package cms341.appcryptor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends AppCompatActivity {
+    Cryptor cryptor;
+    EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        message = (EditText)findViewById(R.id.text_entry);
+        try {
+            cryptor = new Cryptor();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void encryptor(View v) throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
+        String encryptedText = cryptor.encryptText(message.getText().toString(), "thisIsAnEncryptionKeyForThisApp1");
+        message.setText(encryptedText);
+    }
+
+    public void decryptor(View v) throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
+        String decryptedText = cryptor.decryptText(message.getText().toString(), "thisIsAnEncryptionKeyForThisApp1");
+        message.setText(decryptedText);
     }
 }
